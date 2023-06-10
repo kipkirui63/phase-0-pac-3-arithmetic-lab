467dd8020105fd49e35bb5e7479332a58a2edab3
kipkirui63 <kiprutonyachire@gmail.com>
2023-06-10 09:50:20 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+function decrement(n){
+    return n - 1;
+}
+function increment(n){
+    return n + 1;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function makeInt(n){
+    return NaN
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+function preserveDecimal(n){
+    return NaN;
+}
