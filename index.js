5d2e023672b0662b4c6e6c726bfa685af0855150
kipkirui63 <kiprutonyachire@gmail.com>
2023-06-10 10:35:29 +0300

    completed

--- index.js
+++ index.js
@@ -10,33 +10,64 @@ function multiply(){
 function divide(){
 
 }
-function add(a,b){
+// function add(a,b){
+//     return a + b;
+// }
+// function subtract(a,b){
+//     return a - b;
+// }
+// function multiply(a,b){
+//     return a * b;
+// }
+// function divide(a,b){
+//     return a / b;
+// }
+// function decrement(n){
+//     return n - 1;
+// }
+// function increment(n){
+//     return n + 1;
+// }
+// function makeInt(n){
+//     return parseInt(n, 10);
+// }
+// function makeInt(n){
+//     return NaN
+// }
+// function preserveDecimal(n){
+//     return parseFloat(n);
+// }
+// function preserveDecimal(n){
+// //     return NaN;
+// }
+function add(a, b) {
     return a + b;
-}
-function subtract(a,b){
+  }
+  
+  function subtract(a, b) {
     return a - b;
-}
-function multiply(a,b){
+  }
+  
+  function multiply(a, b) {
     return a * b;
-}
-function divide(a,b){
+  }
+  
+  function divide(a, b) {
     return a / b;
-}
-function decrement(n){
+  }
+  
+  function decrement(n) {
     return n - 1;
-}
-function increment(n){
+  }
+  
+  function increment(n) {
     return n + 1;
-}
-function makeInt(n){
+  }
+  
+  function makeInt(n) {
     return parseInt(n, 10);
-}
-function makeInt(n){
-    return NaN
-}
-function preserveDecimal(n){
+  }
+  
+  function preserveDecimal(n) {
     return parseFloat(n);
-}
-function preserveDecimal(n){
-    return NaN;
-}
+  }
